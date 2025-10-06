c74a05cca61f614ce4bc5f38659812f176a1be18
slow126 <spencerlow126@me.com>
2025-10-06 14:31:41 -0600

    update

--- launch_cats_training.sh
+++ launch_cats_training.sh
@@ -6,4 +6,4 @@ python download_cats.py --download_weights
 
 # Run training
 echo "🚀 Starting training..."
-python train_cats.py --train_dataset synthetic --epochs 50 --batch-size 8 --benchmark pfpascal --freeze False --split_to_use_for_validation test
+python train_cats.py --train_dataset synthetic --epochs 50 --batch-size 8 --benchmark pfpascal --freeze True --split_to_use_for_validation test
