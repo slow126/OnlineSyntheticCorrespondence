8104070955f5b9ea23aa95f90a0f3dc09f1ea7f9
slow126 <spencerlow126@me.com>
2025-10-06 22:20:06 -0600

    u

--- launch_cats_training.sh
+++ launch_cats_training.sh
@@ -2,8 +2,8 @@
 
 # Download datasets and weights first
 echo "📦 Downloading datasets and weights..."
-python download_cats.py --download_weights
+python download_cats.py --download_all --download_weights
 
 # Run training
 echo "🚀 Starting training..."
-python train_cats.py --train_dataset synthetic --epochs 50 --batch-size 8 --benchmark pfpascal --freeze True --split_to_use_for_validation test
+python train_cats.py --train_dataset spair --epochs 50 --batch-size 8 --benchmark spair --freeze False --split_to_use_for_validation test
