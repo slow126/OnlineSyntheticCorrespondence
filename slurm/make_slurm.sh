b6f43435a219d309aafd07d525085fb4f4cd7985
slow126 <spencerlow126@me.com>
2025-11-10 20:45:07 -0700

    u

--- slurm/make_slurm.sh
+++ slurm/make_slurm.sh
@@ -1,4 +1,4 @@
-python generate_jobs.py --machine_config slurm/machine_configs/remote.yaml --experiment_config slurm/experiment_configs/pointodyssey_experiment.yaml --output_dir PtOd_jobs
-python generate_jobs.py --machine_config slurm/machine_configs/remote.yaml --experiment_config slurm/experiment_configs/synthetic.yaml --output_dir synthetic_jobs
-python generate_jobs.py --machine_config slurm/machine_configs/remote.yaml --experiment_config slurm/experiment_configs/spair.yaml --output_dir spair_jobs
-python generate_jobs.py --machine_config slurm/machine_configs/remote.yaml --experiment_config slurm/experiment_configs/flyingthings.yaml --output_dir flyingthings_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/pointodyssey_experiment.yaml --output_dir PtOd_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/synthetic.yaml --output_dir synthetic_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/spair.yaml --output_dir spair_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/flyingthings.yaml --output_dir flyingthings_jobs
