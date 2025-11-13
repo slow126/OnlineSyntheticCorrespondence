91d07c92d15bc9841a7a6719bd77c994a8eee7cb
slow126 <spencerlow126@me.com>
2025-11-13 10:17:48 -0700

    u

--- slurm/make_slurm.sh
+++ slurm/make_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Base experiments
-python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/pointodyssey_experiment.yaml --output_dir PtOd_jobs
-python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/synthetic.yaml --output_dir synthetic_jobs
-python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/spair.yaml --output_dir spair_jobs
-python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/flyingthings.yaml --output_dir flyingthings_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/pointodyssey_experiment.yaml --output_dir PtOd_jobs -S
+# python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/synthetic.yaml --output_dir synthetic_jobs -S
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/spair.yaml --output_dir spair_jobs -S
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/flyingthings.yaml --output_dir flyingthings_jobs -S
 
 # Synthetic Views
-python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/synthetic_views.yaml --output_dir synthetic_views_jobs
+python generate_jobs.py --machine_config machine_configs/remote.yaml --experiment_config experiment_configs/synthetic_views.yaml --output_dir synthetic_views_jobs -S
